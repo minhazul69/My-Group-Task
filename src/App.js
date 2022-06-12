ad62e81c338d356358179d7c3aa2e2ee1e3275ce
Minhazul Alam <78158511+akib-chy@users.noreply.github.com>
2022-06-12 09:59:50 +0600

    userflow section added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./Pages/Components/Dashboard/Dashboard";
 import Dashboard1 from "./Pages/Components/Dashboard1/Dashboard1";
+import UserFlow from "./Pages/Components/UserFlow-Section/UserFlow";
 import Footer from "./Pages/Components/Footer/Footer";
 import Navbar from "./Pages/Components/Navbar/Navbar";
 
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Dashboard />
       <Dashboard1 />
+      <UserFlow />
       <Footer />
     </div>
   );
